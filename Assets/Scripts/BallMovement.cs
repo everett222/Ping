93c8a6629a28b0cb2fc3935adf9aa94829bfa82f
everett222 <ewalker@scastudent.ca>
2026-04-24 15:22:26 -0500

    done base game withg ai
    doen and ready to add fun nnnn

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallMovement : MonoBehaviour
+{
+    [SerializeField] private float initialSpeed = 10;
+    [SerializeField] private float speedIncrease = 0.25f;
+    [SerializeField] private Text playerScore;
+    [SerializeField] private Text AIScore;
+
+    private int hitCounter;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Invoke("StartBall", 2f);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = Vector2.ClampMagnitude(rb.linearVelocity, initialSpeed + (speedIncrease * hitCounter));
+    }
+
+    private void StartBall()
+    {
+        rb.linearVelocity = new Vector2(-1, 0) * (initialSpeed + speedIncrease * hitCounter);
+    }
+
+    private void ResetBall()
+    {
+        rb.linearVelocity = new Vector2(0, 0);
+        transform.position = new Vector2(0, 0);
+        hitCounter = 0;
+        Invoke("StartBall", 2f);
+    }
+
+    private void PlayerBounce(Transform myObject)
+    {
+        hitCounter++;
+
+        Vector2 ballPos = transform.position;
+        Vector2 playerPos = myObject.position;
+
+        float xDirection, yDirection;
+        
+        // Check which side of the screen the ball is on to send it the other way
+        if(transform.position.x > 0)
+        {
+            xDirection = -1;
+        }
+        else
+        {
+            xDirection = 1;
+        }
+
+        // Calculate the Y angle based on where the ball hit the paddle
+        yDirection = (ballPos.y - playerPos.y) / myObject.GetComponent<Collider2D>().bounds.size.y;
+        
+        // Prevent the ball from bouncing perfectly straight horizontally
+        if(yDirection == 0)
+        {
+            yDirection = 0.25f;
+        }
+
+        rb.linearVelocity = new Vector2(xDirection, yDirection) * (initialSpeed + (speedIncrease * hitCounter));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the object the ball collided with is named "Player" or "AI"
+        if(collision.gameObject.name == "Player" || collision.gameObject.name == "AI")
+        {
+            PlayerBounce(collision.transform);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(transform.position.x > 0)
+        {
+            ResetBall();
+            playerScore.text = (int.Parse(playerScore.text) + 1).ToString();
+        }
+        else if(transform.position.x < 0)
+        {
+            ResetBall();
+            AIScore.text = (int.Parse(AIScore.text) + 1).ToString();
+        }
+    }
+}
